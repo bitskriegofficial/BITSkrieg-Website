e68b3d70702f78cf79a9e49a6e2ed31b20e6643d
Rhythm Bansal <rskbansal@outlook.com>
2024-02-07 01:36:05 +0530

    fixed title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
                 <BinaryGenerator />
                 <div className='home-content'>
                     <img src="./bitskrieg_logo.png" alt="" id="title" />
-                    <div className='typewriter-home-content'><p>BITS Goa's Cybersecurity Club.</p></div>
+                    <div className='typewriter-home-content'><p>BITS Goa's Cybersecurity Club</p></div>
                 </div>
             </div>
             <div>
